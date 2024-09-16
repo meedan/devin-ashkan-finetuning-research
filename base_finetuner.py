96b44b705a63e5dcc15e46800c360fe2ebd5dc21
Devin Gaffney <itsme@devingaffney.com>
2024-09-16 12:06:57 -0700

    updates

--- base_finetuner.py
+++ base_finetuner.py
@@ -368,9 +368,8 @@ class Finetuner:
         Returns:
             dict: A dictionary containing results for all models and sample sizes.
         """
-        model_names = ['google/gtr-t5-xxl', 'google/gtr-t5-xl', 'google/gtr-t5-large']
+        model_names = ['google/gtr-t5-xl', 'google/gtr-t5-large', 'google/gtr-t5-base']
         overall_results = {}
-
         for model_name in model_names:
             print(f"Starting analysis for model: {model_name}")
             finetuner_instance = Finetuner()
